03b04e6f4796e68b131630fb27031f81f4fe99a2
George Mathew <george.meg91@gmail.com>
2021-03-19 10:08:33 -0400

    More generalization and added F1 Score

--- model_utils.py
+++ model_utils.py
@@ -11,80 +11,60 @@ import itertools
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import f1_score
 
 import logger
 
 LOGGER = logger.get_logger(os.path.basename(__file__.split(".")[0]))
 
 
-class LogReg:
-  def __init__(self, x, y, max_iter=1000):
+class BaseModel:
+  def __init__(self, x, y):
     self.x = x
     self.y = y
+    self.name = "Base Model"
+    self.model = None
+
+  @staticmethod
+  def merge(x):
+    return [list(itertools.chain.from_iterable(x_i)) for x_i in x]
+
+  def fit(self):
+    x = BaseModel.merge(self.x)
+    LOGGER.info("Fitting %s model ... ")
+    self.model.fit(x, self.y)
+
+  def predict(self, x):
+    x = BaseModel.merge(x)
+    return self.model.predict(x)
+
+  def score(self, x, y):
+    x = BaseModel.merge(x)
+    LOGGER.info("Computing accuracy of %s ... " % self.name)
+    return self.model.score(x, y)
+
+  def f1(self, x, y):
+    x = BaseModel.merge(x)
+    LOGGER.info("Computing F1-score of %s ... " % self.name)
+    y_pred = self.model.predict(x)
+    return f1_score(y, y_pred, average='weighted')
+
+class LogReg(BaseModel):
+  def __init__(self, x, y, max_iter=1000):
+    BaseModel.__init__(self, x, y)
+    self.name = "Logistic Regression"
     self.model = LogisticRegression(max_iter=max_iter)
 
-  @staticmethod
-  def merge(x):
-    return [list(itertools.chain.from_iterable(x_i)) for x_i in x]
 
-  def fit(self):
-    x = LogReg.merge(self.x)
-    LOGGER.info("Fitting Logistic Regression model")
-    self.model.fit(x, self.y)
+class KNN(BaseModel):
+  def __init__(self, x, y, k=3):
+    BaseModel.__init__(self, x, y)
+    self.name = "%d - Nearest Neighbors" % k
+    self.model = KNeighborsClassifier(n_neighbors = k)
 
-  def predict(self, x):
-    x = LogReg.merge(x)
-    return self.model.predict(x)
 
-  def score(self, x, y):
-    x = LogReg.merge(x)
-    LOGGER.info("Scoring Logistic Regression model")
-    return self.model.score(x, y)
-
-class Knn:
+class NaiveBayes(BaseModel):
   def __init__(self, x, y):
-    self.x = x
-    self.y = y
-    self.model = KNeighborsClassifier(n_neighbors = 3)
-
-  @staticmethod
-  def merge(x):
-    return [list(itertools.chain.from_iterable(x_i)) for x_i in x]
-
-  def fit(self):
-    x = Knn.merge(self.x)
-    LOGGER.info("Fitting KNN Model")
-    self.model.fit(x, self.y)
-
-  def predict(self, x):
-    x = Knn.merge(x)
-    return self.model.predict(x)
-
-  def score(self, x, y):
-    x = Knn.merge(x)
-    LOGGER.info("Scoring KNN Model")
-    return self.model.score(x, y)
-
-class NaiveBayes:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+    BaseModel.__init__(self, x, y)
+    self.name = "Naive Bayes"
     self.model = GaussianNB()
-
-  @staticmethod
-  def merge(x):
-    return [list(itertools.chain.from_iterable(x_i)) for x_i in x]
-
-  def fit(self):
-    x = NaiveBayes.merge(self.x)
-    LOGGER.info("Fitting Naive Bayes Model")
-    self.model.fit(x, self.y)
-
-  def predict(self, x):
-    x = NaiveBayes.merge(x)
-    return self.model.predict(x)
-
-  def score(self, x, y):
-    x = NaiveBayes.merge(x)
-    LOGGER.info("Scoring Naive Bayes Model")
-    return self.model.score(x, y)
